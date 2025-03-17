84021ee02b8b855ff3c99db37c43435c293ea652
estbs <estbs93@gmail.com>
2025-03-16 23:24:39 -0500

    Add spec to kill all mutants

--- beginner/prime_number/prime_number.rb
+++ beginner/prime_number/prime_number.rb
@@ -2,17 +2,7 @@ module Beginner
   class PrimeNumber
     def is_prime_number?(number)
       return false if number <= 1
-      return true if number <= 3
-
-      return false if number % 2 == 0 || number % 3 == 0
-
-      i = 5
-      while i * i <= number
-        return false if number % i == 0 || number % (i + 2) == 0
-        i += 6
-      end
-
-      true
+      !(2...number).any? { |n| number % n == 0 }
     end
   end
 end
--- spec/beginner/prime_number/prime_number_spec.rb
+++ spec/beginner/prime_number/prime_number_spec.rb
@@ -5,24 +5,14 @@ describe Beginner::PrimeNumber do
   subject { described_class.new }
 
   it 'returns false for numbers less than or equal to 1' do
-    expect(subject.is_prime_number?(-3)).to eq(false)
-    expect(subject.is_prime_number?(0)).to eq(false)
-    expect(subject.is_prime_number?(1)).to eq(false)
+    expect(subject.is_prime_number?(-3)).to be false
+    expect(subject.is_prime_number?(0)).to be false
+    expect(subject.is_prime_number?(1)).to be false
   end
 
   it 'returns true for 2 and 3' do
-    expect(subject.is_prime_number?(2)).to eq(true)
-    expect(subject.is_prime_number?(3)).to eq(true)
-  end
-
-  it 'returns false for even numbers greater than 2' do
-    expect(subject.is_prime_number?(4)).to be false
-    expect(subject.is_prime_number?(10)).to be false
-  end
-
-  it 'returns false for numbers divisible by 3 greater than 3' do
-    expect(subject.is_prime_number?(9)).to be false
-    expect(subject.is_prime_number?(15)).to be false
+    expect(subject.is_prime_number?(2)).to be true
+    expect(subject.is_prime_number?(3)).to be true
   end
 
   it 'returns true for prime numbers' do
@@ -33,6 +23,7 @@ describe Beginner::PrimeNumber do
   end
 
   it 'returns false for non-prime numbers' do
+    expect(subject.is_prime_number?(4)).to be false
     expect(subject.is_prime_number?(6)).to be false
     expect(subject.is_prime_number?(8)).to be false
     expect(subject.is_prime_number?(9)).to be false
@@ -40,10 +31,10 @@ describe Beginner::PrimeNumber do
   end
 
   it 'handles large prime numbers' do
-      expect(subject.is_prime_number?(104729)).to be true
+    expect(subject.is_prime_number?(104729)).to be true
   end
 
   it 'handles large non-prime numbers' do
-      expect(subject.is_prime_number?(104728)).to be false
+    expect(subject.is_prime_number?(104728)).to be false
   end
 end
